6d9be09a7c6ad201d8726164649ae48c18fc33fd
Marcus Whybrow <marcus@whybrow.uk>
2025-05-04 08:24:54 +0100

    feat: derive `Store` and impl `Patch` from reactive_stores for UuidB64

--- src/lib.rs
+++ src/lib.rs
@@ -107,10 +107,13 @@ use crate::errors::{ErrorKind, ResultExt};
 mod errors;
 #[cfg(feature = "serde")]
 mod serde_impl;
+#[cfg(feature = "reactive_stores")]
+mod reactive_stores_impl;
 
 /// It's a Uuid that displays as Base 64
 #[derive(Copy, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 #[cfg_attr(feature = "diesel-uuid", derive(DieselNewType))]
+#[cfg_attr(feature = "reactive_stores", derive(reactive_stores::Store))]
 pub struct UuidB64(uuid::Uuid);
 
 impl UuidB64 {
